26c027d3ec2ee3b0486b40d8897a6627261011e8
paulass97 <paula.skurczynska@gmail.com>
2020-11-14 15:38:28 +0000

    Zad 4

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <math.h>
 
-int main()
-{
-    puts("Hello, World!");
-}
+using namespace std;
+
+//Zad 1.
+class Figura{
+public:
+
+//Zad 2. 
+void setP(double p){p=pole;}
+double getP(){return p;}
+
+private:
+double pole;
+};
+
+//Zad 3. Zad 4.
+class Kolo:public Figura{
+  public:
+  double r;
+  pole = 3.142*r*r;
+};
+
+class Kwadrat:public Figura{
+  public:
+  double a; 
+  pole = a*a;
+}; 
