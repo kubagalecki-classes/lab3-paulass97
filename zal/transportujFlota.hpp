86daea71ddd2c0ba0303e1fb65ea60d75d7ae4ff
paulass97 <73600661+paulass97@users.noreply.github.com>
2020-11-17 02:25:20 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
+    int suma = 0; 
+    int suma_zaglowcow = 0;
+    
+    if(towar==0){
+    return 0;
+    }
+    else{
+    while(suma < towar){
+    Stocznia stocznia{};
+    Statek* s1 = stocznia();
+    towar = s1 -> transportuj();
+    suma+ = towar; 
+    Zaglowiec* suma_zaglowcow = dynamic_cast<Zaglowiec* >(s1);
+    suma_zaglowcow+=Zaglowiec;
+    
+    delete s1;
+        }
+    }
     return 0;
 }
